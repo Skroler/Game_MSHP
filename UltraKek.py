826ea54b0f2ceab3f590c69467b276dca69caee8
DodoKek <57039216+DodoKek@users.noreply.github.com>
2019-10-26 17:59:34 +0300

    Add files via upload
    Квадрат едущий вправо

--- UltraKek.py
+++ UltraKek.py
@@ -1,64 +1,39 @@
 import pygame as pg
 import sys
-import random
+import random as rand
 import math
 
-size = width, height = 1024, 768
+size = width, height = 1290, 720
 black = 0, 0, 0
+red = 255, 0, 0
 
 
 def main():
     pg.init()
     screen = pg.display.set_mode(size)
     gameover = False
-    alpha  = 0
-    x = 10
-    y = 10
-    kek = 0
+    x1 = 0 #нач координаты левого угла
+    y1 = 0 #--
 
+    x2 = 30 #Размеры
+    y2 = 30 #--
+
+    vX = 30 #нач скорость по X
+    vY = 0  #по Y
     while not gameover:
-
+            screen.fill(black)
             for event in pg.event.get():
                     if event.type == pg.QUIT:
                         gameover = True
 
-                    if(kek == 0):
-                        if event.type == pg.KEYDOWN:
-                            if event.key == pg.K_LEFT:
-                                x -= 30
+            pg.draw.rect(screen, red, (x1, y1, x2, y2), 0) #отрисовка головы змейки
 
-                        if event.type == pg.KEYDOWN:
-                            if event.key == pg.K_RIGHT:
-                                x += 30
+            x1 += vX
+            y1 += vY
 
-                        if event.type == pg.KEYDOWN:
-                            if event.key == pg.K_DOWN:
-                                y += 30
-
-                        if event.type == pg.KEYDOWN:
-                            if event.key == pg.K_UP:
-                                y -= 30
-            if y > 600 :
-                kek = 1
-
-
-            screen.fill(black)
-            plane = pg.image.load("plane.png")
-            plane = pg.transform.scale(plane, (width//5, height//5))
-            planerect = plane.get_rect()
-            planerect.x = x
-            planerect.y = y
-            #alpha -= 0.05
-            screen.blit(plane, planerect)
-            if kek == 1:
-                font = pg.font.SysFont('Comic Sans MS', 105, True)
-                data = '11.09.2001'
-                ts = font.render(data, False, (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
-                screen.blit(ts, (250, 300))
 
             pg.display.flip()
-
-            pg.time.wait(10)
+            pg.time.wait(1000)
     sys.exit()
 
 if __name__ == '__main__':
